296836a0cd3ae8e56d74a2f1026cc0c973253a3b
Caio <caio.student@outlook.com>
2020-05-29 21:16:56 -0300

    identation

--- Case1/Grafico 2- Eficiência de filtro.py
+++ Case1/Grafico 2- Eficiência de filtro.py
@@ -1,11 +1,14 @@
-from pylab import *   
-from Classe_RNA import RNA
-from Classe_Filtro import Filtro
-from Classe_Equação import Equação
-from Classe_Entropia import Entropia
+from pylab              import *   
+from Classe_RNA         import RNA
+from Classe_Filtro      import Filtro
+from Classe_Equação     import Equação
+from Classe_Entropia    import Entropia
+
 #Entrada de dados...
-mol=500#int(input('Quantidade de molécula:'))
-tam=50#int(input('Tamanho da molécula:'))
+#int(input('Quantidade de molécula:'))
+mol=500
+#int(input('Tamanho da molécula:'))
+tam=50
 #Taxa de mutação (%):
 alfa=5
 #Eficiência de filtro(%):
@@ -18,44 +21,44 @@ limite=1000
 popMax=500
 
 #Aplicando classes e funçoes...
-moleculas=RNA()
-filtro=Filtro()
-calculado=Equação()
-entropia=Entropia()
-ciclo=0
-afinidade=0
+moleculas   = RNA()
+filtro      = Filtro()
+calculado   = Equação()
+entropia    = Entropia()
+ciclo       = 0
+afinidade   = 0
 
-lista_afinidade=[0]
-lista_ciclo=[0]
-lista_tamanho=[50]
-lista_QTD=[0]
-lista_entropia=[2]
+lista_afinidade = [ 0 ]
+lista_ciclo     = [ 0 ]
+lista_tamanho   = [ 50 ]
+lista_QTD       = [ 0 ]
+lista_entropia  = [ 2 ]
 
 #Gerando as primeiras moleculas:
 moleculas_1G , afinidade_1G= moleculas.Gerar(mol, tam, alvo)
-m_gerais=moleculas_1G
-NA= afinidade_1G*(len(moleculas_1G))
-NB=(len(moleculas_1G)) - NA
+m_gerais = moleculas_1G
+NA = afinidade_1G*(len(moleculas_1G))
+NB = (len(moleculas_1G)) - NA
 
-while(afinidade <1 and ciclo< limite ):
+while(afinidade < 1 and ciclo < limite ):
     #Para replicação
-    m_duplicadas=moleculas.PCR(m_gerais ,alfa)
+    m_duplicadas = moleculas.PCR(m_gerais ,alfa)
      
     #Para Constant Population
     m_ordenadas = moleculas.CP(m_duplicadas,popMax,alvo)
 
     #Para Filtragem das moleculas
-    m_filtrada, afinidade, TAM=filtro.seleção(m_ordenadas, alvo, beta)
+    m_filtrada, afinidade, TAM = filtro.seleção(m_ordenadas, alvo, beta)
 
     #Para Entropia de Shannon
-    bits=entropia.Shannon(m_filtrada)
+    bits = entropia.Shannon(m_filtrada)
 
     #Para Equação de Crescimento
-    QTD_C , AFF_C, NA, NB=calculado.Crescimento( NA, NB, alfa, beta, popMax)
+    QTD_C , AFF_C, NA, NB = calculado.Crescimento( NA, NB, alfa, beta, popMax)
 
-    QTD=len(m_filtrada)
+    QTD = len(m_filtrada)
     m_gerais = m_filtrada
-    ciclo+=1
+    ciclo+= 1
 
     #Para exibição de cada ciclo
     print('\nCiclo: %d'%ciclo)
@@ -72,12 +75,12 @@ while(afinidade <1 and ciclo< limite ):
  
 
 #dados
-soma1=0
-d=0
+soma1 = 0
+d = 0
 for i in range(99,999):
-    soma1+=lista_afinidade[i]
-    d+=1
-print('Afinidade média: %.2f'%(soma1/d))   
+    soma1+= lista_afinidade[ i ]
+    d+= 1
+print('Afinidade média: %.2f'%( soma1 / d ))   
 # Gerando 4 graficos 
     
 #Primeiro (Afinidade X Ciclo) 
@@ -90,7 +93,7 @@ plt.rcParams['figure.figsize'] = (12,8)
 
 plt.subplot(2,2,1)
 plt.title('AFINIDADE (mol. afins)')
-plt.plot( grafico1_x , grafico1_y, c='#0000ff',lw=3)
+plt.plot( grafico1_x , grafico1_y, c ='#0000ff',lw = 3)
 plt.xlabel('Ciclo/Round')
 plt.ylabel('Afinidade(%)')
 
@@ -102,7 +105,7 @@ grafico2_y = lista_QTD
 
 plt.subplot(2,2,2)
 plt.title('QUANTIDADE DE MOLÉCULAS')
-plt.plot( grafico2_x , grafico2_y, c='#0000ff',lw=3)
+plt.plot( grafico2_x , grafico2_y, c = '#0000ff',lw = 3)
 plt.xlabel('Ciclo/Round')
 plt.ylabel('QTD_moléculas')
 plt.grid(True)
@@ -113,7 +116,7 @@ grafico3_y = lista_entropia
 
 plt.subplot(2,2,3)
 plt.title('ENTROPIA MÉDIA DAS MOLÉCULAS (Bits)')
-plt.plot( grafico3_x , grafico3_y, c='#0000ff',lw=3 )
+plt.plot( grafico3_x , grafico3_y, c = '#0000ff',lw = 3 )
 plt.xlabel('Ciclo/Round')
 plt.ylabel('Entropia')
 plt.grid(True)
@@ -124,7 +127,7 @@ grafico4_y = lista_tamanho
 
 plt.subplot(2,2,4)
 plt.title('TAMANHO MÉDIO DAS MOLÉCULAS (Base)')
-plt.plot( grafico4_x , grafico4_y, c='#0000ff',lw=3 )
+plt.plot( grafico4_x , grafico4_y, c = '#0000ff',lw = 3 )
 plt.xlabel('Ciclo/Round')
 plt.ylabel('Tamanho Médio')
 plt.grid(True)
