cebe1c6f444acd6180f2228c58f3555da4fadaea
Caio <caio.student@outlook.com>
2020-06-11 20:23:15 -0300

    refactory3

--- Case1/EntropyClass.py
+++ Case1/EntropyClass.py
@@ -1,31 +1,34 @@
 import math
-class Entropia:
-    def Shannon( self, moleculas ):
-        self.moleculas = moleculas
-        b = [ 'A','C','T','G' ]
+
+class ShannonEntropy:
+    def __init__(self):
+        self.result_entropy = [2]
+        self.b = [ 'A','C','T','G' ]
+    def Result(self, molecules):
+        molecules
         entropia = []
         entropia_media = 0
         
-        for i in range( len( self.moleculas[ 0 ] ) ):
+        for i in range( len( molecules[ 0 ] ) ):
             a = 0
             c = 0
             t = 0
             g = 0
             
-            for sequencia in self.moleculas:
-                if (sequencia[ i ] == b[ 0 ] ):
+            for sequencia in molecules:
+                if (sequencia[ i ] == self.b[ 0 ] ):
                     a+= 1
-                elif(sequencia[ i ] == b[ 1 ] ):
+                elif(sequencia[ i ] == self.b[ 1 ] ):
                     c+= 1
-                elif(sequencia[ i ] == b[ 2 ] ):
+                elif(sequencia[ i ] == self.b[ 2 ] ):
                     t+= 1
-                elif(sequencia[ i ] == b[ 3 ]):
+                elif(sequencia[ i ] == self.b[ 3 ]):
                     g+= 1
 
-            a = a / ( len( self.moleculas ) )
-            c = c / ( len( self.moleculas ) )
-            t = t / ( len( self.moleculas ) )
-            g = g / ( len( self.moleculas ) )
+            a = a / ( len( molecules ) )
+            c = c / ( len( molecules ) )
+            t = t / ( len( molecules ) )
+            g = g / ( len( molecules ) )
             if( a == 0 ):                                            #Caso alguma das letras der 0, colocar 1 pois se nao da erro(nao faz diferença pois log2(1)=0)
                 a = 1
             if( c == 0 ):
@@ -40,4 +43,11 @@ class Entropia:
 
 
         entropia_media = sum( entropia ) / len( entropia )
+        self.result_entropy.append(entropia_media)
         return entropia_media
+
+
+'''
+NOTA
+Função faz entropia, retorna a entropia do clico atual e armazena um array de entropia para cada ciclo
+'''
